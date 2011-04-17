01c0be21b339275565b986e1c5a2df0da6c30838
Bertrand Lorentz <bertrand.lorentz@gmail.com>
2011-04-17 10:47:22 +0200

    Update banshee package to 2.0.0

--- packages/banshee.py
+++ packages/banshee.py
@@ -1,9 +1,9 @@
 class BansheePackage (Package):
 	def __init__ (self):
-		Package.__init__ (self, 'banshee-1', '1.9.4')
+		Package.__init__ (self, 'banshee', '2.0.0')
 
 		self.sources = [
-			'http://download.banshee.fm/banshee/unstable/%{version}/%{name}-%{version}.tar.bz2'
+			'http://download.banshee.fm/banshee/stable/%{version}/%{name}-%{version}.tar.bz2'
 		]
 
 #		self.configure = './autogen.sh --prefix=%{prefix}'
